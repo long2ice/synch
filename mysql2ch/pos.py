ed40c10b25a675d09f63057d3c56b6aa1d64c7e4
long2ice <long2ice@prismslight.com>
2020-02-24 21:41:13 +0800

    fix pos error exec_master_log_pos

--- mysql2ch/pos.py
+++ mysql2ch/pos.py
@@ -28,7 +28,7 @@ class FileLogPos(LogPos):
         self.config.set('log_position', 'master_host', master_host)
         self.config.set('log_position', 'master_port', str(master_port))
         self.config.set('log_position', 'relay_master_log_file', relay_master_log_file)
-        self.config.set('log_position', 'exec_master_log_pos', exec_master_log_pos)
+        self.config.set('log_position', 'exec_master_log_pos', str(exec_master_log_pos))
         self.config.write(open(self.file, 'w'))
 
     def set_log_pos_slave(self, log_file, log_pos):
