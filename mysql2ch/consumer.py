be63497720d53825ed3687b98632c3c775cb2fc2
long2ice <long2ice@prismslight.com>
2020-04-03 11:22:09 +0800

    fix many lag

--- mysql2ch/consumer.py
+++ mysql2ch/consumer.py
@@ -29,18 +29,30 @@ def consume(args):
     )
     topic = settings.KAFKA_TOPIC
     partition = settings.PARTITIONS.get(group_id)
-    consumer.assign([TopicPartition(topic, partition)])
+    tp = TopicPartition(topic, partition)
+    consumer.assign([tp])
+
     event_list = []
+    is_insert = False
+
     logger.info(f'success consume topic:{topic},partition:{partition},schema:{schema},table:{table}')
+
     pk = reader.get_primary_key(schema, table)
+
     for msg in consumer:  # type:ConsumerRecord
         logger.debug(f'kafka msg:{msg}')
         event = msg.value
         event_list.append(event)
         len_event = len(event_list)
-        if len_event == settings.INSERT_NUMS or (
-                (int(time.time() * 10 ** 6) - event_list[0][
-                    'event_unixtime']) / 10 ** 6 >= settings.INSERT_INTERVAL > 0):
+        high_water = consumer.highwater(tp)
+        lag = (high_water - 1) - msg.offset
+        if lag > settings.INSERT_NUMS:
+            if len_event == settings.INSERT_NUMS:
+                is_insert = True
+        else:
+            if (int(time.time() * 10 ** 6) - event_list[0]['event_unixtime']) / 10 ** 6 >= settings.INSERT_INTERVAL > 0:
+                is_insert = True
+        if is_insert:
             data_dict = {}
             tmp_data = []
             for items in event_list:
@@ -53,6 +65,7 @@ def consume(args):
             result = writer.insert_event(tmp_data, settings.SKIP_TYPE, settings.SKIP_DELETE_TB_NAME, schema, table, pk)
             if result:
                 event_list = []
+                is_insert = False
                 consumer.commit()
                 logger.info(f'commit success {len_event} events!')
             else:
