e486bcf6398adebc2df7fe00d60e1b011a547939
long2ice <long2ice@prismslight.com>
2020-04-09 16:27:18 +0800

    fix etl full

--- mysql2ch/replication.py
+++ mysql2ch/replication.py
@@ -18,12 +18,12 @@ def etl_full(schema, tables, renew=False):
             try:
                 writer.execute(drop_sq)
                 logger.info(f'drop table success:{schema}.{table}')
-                try:
-                    sql = f"CREATE TABLE {schema}.{table} ENGINE = MergeTree ORDER BY {pk} AS SELECT * FROM mysql('{settings.MYSQL_HOST}:{settings.MYSQL_PORT}', '{schema}', '{table}', '{settings.MYSQL_USER}', '{settings.MYSQL_PASSWORD}')"
-                    writer.execute(sql)
-                    writer.fix_table_column_type(reader, schema, table)
-                    logger.info(f'etl success:{schema}.{table}')
-                except Exception as e:
-                    logger.error(f'etl error:{schema}.{table},{e}')
             except Exception as e:
                 logger.error(f'etl error with renew:{schema}.{table},{e}')
+        try:
+            sql = f"CREATE TABLE {schema}.{table} ENGINE = MergeTree ORDER BY {pk} AS SELECT * FROM mysql('{settings.MYSQL_HOST}:{settings.MYSQL_PORT}', '{schema}', '{table}', '{settings.MYSQL_USER}', '{settings.MYSQL_PASSWORD}')"
+            writer.execute(sql)
+            writer.fix_table_column_type(reader, schema, table)
+            logger.info(f'etl success:{schema}.{table}')
+        except Exception as e:
+            logger.error(f'etl error:{schema}.{table},{e}')
