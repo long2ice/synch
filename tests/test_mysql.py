855a6abdbc94a327d39307447cddd1ca063f925e
long2ice <long2ice@gmail.com>
2020-07-06 19:00:12 +0800

    skip test_get_binlog

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -1,3 +1,6 @@
+import pytest
+
+from conftest import local
 from synch.factory import get_reader
 
 
@@ -7,6 +10,7 @@ def test_get_pk():
     assert ret == "id"
 
 
+@pytest.mark.skipif(not local, reason="can't get binlog when not local")
 def test_get_binlog():
     reader = get_reader("mysql_db")
     ret = reader.get_binlog_pos()
