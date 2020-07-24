fa9d9a5b9d28bcb2f457f9123cab730e27d09ea1
long2ice <long2ice@gmail.com>
2020-07-24 11:26:17 +0800

    update version

--- synch/cli.py
+++ synch/cli.py
@@ -18,7 +18,7 @@ def version():
     # with open("pyproject.toml") as f:
     #     ret = re.findall(r'version = "(\d+\.\d+\.\d+)"', f.read())
     #     return ret[0]
-    return "0.6.6"
+    return "0.6.7"
 
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
